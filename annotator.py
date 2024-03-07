0eace39eb12029c93cd1f3871451dac06bbe561c
reddiedev <105657266+reddiedev@users.noreply.github.com>
2024-03-07 17:22:36 +0800

    init setup of frontend

--- annotator.py
+++ annotator.py
@@ -4,41 +4,127 @@ import pygame.camera
 import pygame
 import datetime
 import pygame_gui
+from pygame_gui.core import ObjectID
+from pygame_gui.elements import UIButton
 
 # config variables
-WIDTH = 1280
-HEIGHT = 720
-BACKGROUND_COLOR = '#000000'
+screen_width = 1280
+screen_height = 720
+screen_margins = 25
+background_color = '#000000'
+
+button_width = 100
+button_height = 50
 
 # pygame setup
-pygame.camera.init()
 pygame.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-background = pygame.Surface((WIDTH, HEIGHT))
-background.fill(pygame.Color(BACKGROUND_COLOR))
+
+
 pygame.display.set_caption("NLNT Turtlebot Data Annotator Studio")
-manager = pygame_gui.UIManager((WIDTH, HEIGHT))
+icon_image = pygame.image.load("media/annotator-icon.png")
+pygame.display.set_icon(icon_image)
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+background = pygame.Surface((screen_width, screen_height))
+background.fill(pygame.Color(background_color))
+manager = pygame_gui.UIManager((screen_width, screen_height), "theme.json")
 clock = pygame.time.Clock()
+
+# initiate camera
+pygame.camera.init()
+camlist = pygame.camera.list_cameras()
+if not camlist:
+    print("ERROR: No cameras detected")
+camera_index = 1  # If your device has multiple cameras, adjust to the correct one
+cam = pygame.camera.Camera(camlist[camera_index], (1280, 720))
+cam.start()
+image = cam.get_image()
 
 # state variables setup
 is_running = True
 """ (bool): if the program is running or not """
 time_delta = 0
+tick_counter = 0
 
 # global variables
+sensor_text = "Lorem Ipsum"
+
+# UI elements
+start_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((screen_width-(screen_margins+button_width)*4, screen_height-screen_margins-button_height), (button_width, button_height)),
+                                            text='Start',
+                                            manager=manager, object_id=ObjectID(class_id='@rounded_buttons',
+                                                                                object_id='#start_button'), tool_tip_text="start recording data")
+
+stop_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((screen_width-(screen_margins+button_width)*3, screen_height-screen_margins-button_height), (button_width, button_height)),
+                                           text='Stop',
+                                           manager=manager, object_id=ObjectID(class_id='@rounded_buttons',
+                                                                               object_id='#stop_button'), tool_tip_text="stop recording data")
+
+clear_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((screen_width-(screen_margins+button_width)*2, screen_height-screen_margins-button_height), (button_width, button_height)),
+                                            text='Clear',
+                                            manager=manager, object_id=ObjectID(class_id='@rounded_buttons',
+                                                                                object_id='#clear_button'), tool_tip_text="clear input field")
+
+save_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((screen_width-(screen_margins+button_width), screen_height-screen_margins-button_height), (button_width, button_height)),
+                                           text='Save',
+                                           manager=manager, object_id=ObjectID(class_id='@rounded_buttons',
+                                                                               object_id='#save_button'), tool_tip_text="save annotated data to output file")
+
+data_text = pygame_gui.elements.UITextEntryBox(relative_rect=pygame.Rect((652.5, 275), (602.5, 350)),
+                                               initial_text=sensor_text,
+                                               manager=manager)
+
+image_box = pygame_gui.elements.UIImage(relative_rect=pygame.Rect((25, 275), (602.5, 350)),
+                                        image_surface=image,
+                                        manager=manager,)
+
+prompt_box = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect((25, 175), (1230, 75)),
+                                                 placeholder_text="Enter prompt here...",
+                                                 manager=manager,)
 
 
-# BUTTONS
-hello_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((350, 275), (100, 50)),
-                                            text='Say Hello',
-                                            manager=manager)
+user_box = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect((25, 100), (150, 50)),
+                                               placeholder_text="username",
+                                               manager=manager,)
 
+instance_box = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect((200, 100), (150, 50)),
+                                                   placeholder_text="instance_id",
+                                                   manager=manager,)
+
+connection_text = pygame_gui.elements.UILabel(relative_rect=pygame.Rect((25, 25), (300, 50)),
+                                              text="Status: CONNECTED Ping: 20 ms",
+                                              manager=manager, object_id=ObjectID(class_id='@text_input',
+                                                                                  object_id='#connection_text'),)
+
+
+battery_text = pygame_gui.elements.UILabel(relative_rect=pygame.Rect((725, 25), (125, 50)),
+                                           text="Battery: 100%",
+                                           manager=manager, object_id=ObjectID(class_id='@text_input',
+                                                                               object_id='#battery_text'),)
+
+time_text = pygame_gui.elements.UILabel(relative_rect=pygame.Rect((1075, 25), (200, 50)),
+                                        text="Time:",
+                                        manager=manager, object_id=ObjectID(class_id='@text_input',
+                                                                            object_id='#battery_text'),)
 
 while is_running:
     time_delta = clock.tick(60) / 1000  # limits FPS to 60
 
-    current_time = datetime.datetime.now()
-    unix_timestamp = int(current_time.timestamp())
+    # update every tick
+    if (tick_counter > 0):
+        current_time = datetime.datetime.now()
+        unix_timestamp = int(current_time.timestamp())
+        time_text.set_text(f'Time: {unix_timestamp}')
+
+    # update image output every 2 ticks (30 fps video feed at 60 fps game output)
+    if (tick_counter % 2 == 0):
+        tick_counter = 0
+        image = cam.get_image()
+        image_box.set_image(image)
+
+    # reset tick every 60
+    if (tick_counter == 59):
+        tick_counter = 0
 
     # event handler
     for event in pygame.event.get():
@@ -46,8 +132,14 @@ while is_running:
             is_running = False
 
         if event.type == pygame_gui.UI_BUTTON_PRESSED:
-            if event.ui_element == hello_button:
-                print('Hello World!')
+            if event.ui_element == start_button:
+                print('Start!')
+            elif event.ui_element == stop_button:
+                print('Stop!')
+            elif event.ui_element == clear_button:
+                print('Clear!')
+            elif event.ui_element == save_button:
+                print('Save!')
 
         manager.process_events(event)
 
@@ -57,6 +149,7 @@ while is_running:
     manager.draw_ui(screen)
 
     pygame.display.update()
+    tick_counter += 1
 
 
 pygame.quit()
