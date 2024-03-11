2961cb1c29e5b4e1e0d9bd8741a23db855569fb1
reddiedev <105657266+reddiedev@users.noreply.github.com>
2024-03-11 16:49:35 +0800

    update latest

--- workspace/src/sensors/sensors/annotator.py
+++ workspace/src/sensors/sensors/annotator.py
@@ -1,13 +1,17 @@
 # imports
 from pygame.locals import *
-import pygame.camera
 import pygame
+import pygame.camera
 import datetime
 import pygame_gui
 from pygame_gui.core import ObjectID
 from pygame_gui.elements import UIButton, UITextEntryBox, UILabel, UIImage, UITextEntryLine
 import json
 import os
+from KNetworking import DataBridgeServer_TCP
+import threading
+import time
+
 
 if not os.path.exists("datalogs"):
     os.mkdir("datalogs")
@@ -23,35 +27,20 @@ button_height = 50
 
 
 class Annotator:
-    def __init__(self) -> None:
-        # pygame setup
-        self.pygame = pygame
-        self.pygame.init()
+    def __init__(self, manual_control=True) -> None:
+        self.data_buffer = None
+        self.gathering_data = False
 
-        # annotator app setup
-        self.pygame.display.set_caption("NLNT Turtlebot Data Annotator Studio")
-        icon_image = self.pygame.image.load("media/annotator-icon.png")
-        self.pygame.display.set_icon(icon_image)
+        # will be useful once data gathering autmation is started
+        self.manual_control = manual_control
 
-        # UI setup
-        self.screen = self.pygame.display.set_mode(
-            (screen_width, screen_height))
-        self.background = self.pygame.Surface((screen_width, screen_height))
-        self.background.fill(self.pygame.Color(background_color))
-        self.manager = pygame_gui.UIManager(
-            (screen_width, screen_height), "theme.json")
-        self.clock = self.pygame.time.Clock()
-
-        # initiate camera
-        self.pygame.camera.init()
-        camlist = self.pygame.camera.list_cameras()
-        if not camlist:
-            print("ERROR: No cameras detected")
-        camera_index = 1  # If your device has multiple cameras, adjust to the correct one
-        self.cam = self.pygame.camera.Camera(
-            camlist[camera_index], (1280, 720))
-        self.cam.start()
-        self.image = self.cam.get_image()
+        # keyboard stuff
+        self.keyboard_input = None
+        self.keyboard_impulse = False
+        self.killswitch = False
+        self.debug = True
+        self.debug_window_process = True
+        self.label = None
 
         # state variables setup
         self.is_running = True
@@ -74,11 +63,39 @@ class Annotator:
         """enter sensor data into this string"""
         # TODO: save sensor data into the above global variables
 
-    def start(self):
-        self.setupInterfaceElements()
+        # USE IN MACOS
         self.showInterface()
 
-    def setupInterfaceElements(self):
+        # USE IN UBUNTU
+        # self.window_thread = threading.Thread(
+        #     target=self.showInterface)
+        # self.window_thread.start()
+        print(f'User interface initiated')
+
+        self.server_data_receiver = DataBridgeServer_TCP(port_number=50000)
+        print(f'Server Listening on port 50000')
+
+        self.movement_data_sender = DataBridgeServer_TCP(port_number=50001)
+        print(f'Server Listening on port 50001')
+
+        self.data_listener_thread = threading.Thread(
+            target=self.super_json_listener)
+        self.data_listener_thread.start()
+
+    def super_json_listener(self):
+
+        while True:
+
+            if not self.gathering_data:
+                time.sleep(0.007)
+                continue
+            data = self.server_data_receiver.receive_data().decode()
+            if self.data_buffer == None:
+                print("WARNING: data buffer is still None type.")
+                continue
+            self.data_buffer.append(data)
+
+    def initializeElements(self):
         # UI elements
         self.start_button = UIButton(relative_rect=pygame.Rect((screen_width-(screen_margins+button_width)*4, screen_height-screen_margins-button_height), (button_width, button_height)),
                                      text='Start',
@@ -131,13 +148,46 @@ class Annotator:
                                                                               object_id='#battery_text'),)
 
         self.time_label = UILabel(relative_rect=pygame.Rect((1075, 25), (200, 50)),
-                                  text=f"Time: {
-            int(datetime.datetime.now().timestamp())}",
-            manager=self.manager, object_id=ObjectID(class_id='@text_input',
-                                                     object_id='#battery_text'),)
+                                  text=f'Time: {int(datetime.datetime.now().timestamp())}',
+                                  manager=self.manager, object_id=ObjectID(class_id='@text_input',
+                                                                           object_id='#battery_text'))
 
     def showInterface(self):
 
+        # PYGAME INIT START
+        self.pygame = pygame
+        self.pygame.init()
+
+        # annotator app setup
+        self.pygame.display.set_caption("NLNT Turtlebot Data Annotator Studio")
+        icon_image = self.pygame.image.load("media/annotator-icon.png")
+        self.pygame.display.set_icon(icon_image)
+
+        # UI setup
+        self.screen = self.pygame.display.set_mode(
+            (screen_width, screen_height))
+        self.background = self.pygame.Surface((screen_width, screen_height))
+        self.background.fill(self.pygame.Color(background_color))
+        self.manager = pygame_gui.UIManager(
+            (screen_width, screen_height), "theme.json")
+        self.clock = self.pygame.time.Clock()
+
+        # initiate camera
+        self.pygame.camera.init()
+        camlist = self.pygame.camera.list_cameras()
+        if not camlist:
+            print("ERROR: No cameras detected")
+        camera_index = 1  # If your device has multiple cameras, adjust to the correct one
+        self.cam = self.pygame.camera.Camera(
+            camlist[camera_index], (1280, 720))
+        self.cam.start()
+        self.image = self.cam.get_image()
+
+        # PYGAME INIT END
+
+        self.initializeElements()
+
+        # main loop
         while self.is_running:
             time_delta = self.clock.tick(60) / 1000  # limits FPS to 60
 
@@ -164,6 +214,20 @@ class Annotator:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.is_running = False
+
+                if event.type == pygame.KEYDOWN:
+                    if event.scancode == 79:
+                        # RIGHT
+                        print('[events/keyPressed]: d key pressed!')
+                    if event.scancode == 80:
+                        # LEFT
+                        print('[events/keyPressed]: a key pressed!')
+                    if event.scancode == 81:
+                        # DOWN
+                        print('[events/keyPressed]: s key pressed!')
+                    if event.scancode == 82:
+                        # UP
+                        print('[events/keyPressed]: d key pressed!')
 
                 if event.type == pygame_gui.UI_BUTTON_PRESSED:
                     if event.ui_element == self.start_button:
@@ -212,17 +276,6 @@ class Annotator:
 
                 self.manager.process_events(event)
 
-            # keyboard press handler
-            keys = pygame.key.get_pressed()
-            if keys[pygame.K_w]:
-                print('[events/keyPressed]: w key pressed!')
-            if keys[pygame.K_s]:
-                print('[events/keyPressed]: s key pressed!')
-            if keys[pygame.K_a]:
-                print('[events/keyPressed]: a key pressed!')
-            if keys[pygame.K_d]:
-                print('[events/keyPressed]: d key pressed!')
-
             self.manager.update(time_delta)
             self.screen.blit(self.background, (0, 0))
             self.manager.draw_ui(self.screen)
@@ -234,4 +287,3 @@ class Annotator:
 
 
 annotator = Annotator()
-annotator.start()
