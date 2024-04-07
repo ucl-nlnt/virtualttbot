dc82821e5e1971fd269322c14ddf70ca43c397c0
PatchouliPatch <gnkaquilala@up.edu.ph>
2024-04-07 14:33:13 +0800

    added data.7z for data processing tests

--- workspace/src/sensors/sensors/data_test.py
+++
@@ -1,7 +0,0 @@
-# dont mind this, this was made to check if the changes still reflected even after getting an std err
-
-def main(args = None):
-    print(2)
-
-if __name__ == '__main__':
-    main()
