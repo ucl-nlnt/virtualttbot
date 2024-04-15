a1b1e9a12c5acdd690f0c8326063e50a146de033
marae <mara.escano123@gmail.com>
2024-04-15 22:36:58 +0800

    agzzz

--- agz_quickstart.py
+++ agz_quickstart.py
@@ -1,7 +1,7 @@
 # Gazebo Quickstart
 
 import subprocess
-import time
+import getch
 
 # For Custom Worlds
 world = input("Enter World: ")
@@ -15,4 +15,10 @@ launch_gazebo = subprocess.Popen('export TURTLEBOT3_MODEL=burger ; ros2 launch t
 
 # Launch Pygame
 cmd_pygame = 'python3 virtualttbot/workspace/src/sensors/sensors/auto_data_collector_lv12.py --name ' + name
-launch_pygame = subprocess.Popen(cmd_pygame, shell=True)
+launch_pygame = subprocess.Popen(cmd_pygame, stdin= subprocess.PIPE, stderr=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
+
+# capture stdout and stderr
+while True:
+	x = getch.getch()
+	out, err = launch_pygame.communicate(x)
+	out, err = launch_gazebo.communicate(x)
