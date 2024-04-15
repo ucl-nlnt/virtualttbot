be56eb8bec3221ec59783c0aab2fed38edb4f511
marae <mara.escano123@gmail.com>
2024-04-15 21:07:42 +0800

    sjdjkbhdskj

--- agz_quickstart.py
+++ agz_quickstart.py
@@ -14,5 +14,5 @@ if world == "":
 launch_gazebo = subprocess.Popen('export TURTLEBOT3_MODEL=burger ; ros2 launch turtlebot3_gazebo ' + world + '.launch.py', stdout=subprocess.DEVNULL, shell=True)
 
 # Launch Pygame
-cmd_pygame = 'python3 ~/virtualttbot/workspace/src/sensors/sensors/auto_data_collector_lv12.py --name ' + name
+cmd_pygame = 'python3 virtualttbot/workspace/src/sensors/sensors/auto_data_collector_lv12.py --name ' + name
 launch_pygame = subprocess.Popen(cmd_pygame, shell=True)
