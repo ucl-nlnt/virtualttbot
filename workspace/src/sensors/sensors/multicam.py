399cecfad16cf6e8d8abc0248e8b3e5d5f8e42f6
PatchouliPatch <gnkaquilala@up.edu.ph>
2024-04-26 12:25:57 +0800

    added camera test script multicam.py

---
+++ workspace/src/sensors/sensors/multicam.py
@@ -0,0 +1,43 @@
+import cv2
+import sys
+
+def retrieve_camera_indexes():
+
+    max_tested = 10
+    num_cameras = 0
+    usable_camera_indexes = []
+    for i in range(max_tested):
+        cap = cv2.VideoCapture(i)
+        if cap is None or not cap.isOpened():
+            cap.release()
+            continue
+        else:
+            usable_camera_indexes.append(i)
+        num_cameras += 1
+        cap.release()
+        
+    return usable_camera_indexes
+
+print("Available Cameras:", retrieve_camera_indexes())
+
+cap = cv2.VideoCapture(2)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH,1280)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT,720)
+
+cap2 = cv2.VideoCapture(0)
+
+while True:
+    
+    ret, frame = cap.read()
+    if not ret:
+        continue
+
+    ret, frame2 = cap2.read()
+
+    cv2.imshow('Webcam', frame)
+    cv2.imshow('Webcam main', frame2)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
